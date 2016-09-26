fb7f9e028f21be4b1dcde3571e0d47ae7e59486a
mostafa_anter <ga30000@gmail.com>
2016-09-26 22:45:06 +0200

    stable version

---
+++ app/src/main/java/com/perfect_apps/koch/activities/SignInActivity.java
@@ -0,0 +1,22 @@
+package com.perfect_apps.koch.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.perfect_apps.koch.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
