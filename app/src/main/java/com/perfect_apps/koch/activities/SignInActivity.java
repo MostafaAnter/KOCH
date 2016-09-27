22460bf4b68c5a327c909338ab98d157988c72a6
mostafa_anter <ga30000@gmail.com>
2016-09-27 11:16:07 +0200

    stable version

--- app/src/main/java/com/perfect_apps/koch/activities/SignInActivity.java
+++ app/src/main/java/com/perfect_apps/koch/activities/SignInActivity.java
@@ -1,22 +1,42 @@
 package com.perfect_apps.koch.activities;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.akexorcist.localizationactivity.LocalizationActivity;
 import com.perfect_apps.koch.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SignInActivity extends LocalizationActivity {
+
+    @BindView(R.id.radioButton1) RadioButton radioButton1;
+    @BindView(R.id.radioButton2) RadioButton radioButton2;
+
+    @BindView(R.id.username_input) EditText userNameInput;
+    @BindView(R.id.password_input) EditText passwordInput;
+
+    @BindView(R.id.loginButton) Button loginButton;
+
+    @BindView(R.id.text1) TextView textView1;
+    @BindView(R.id.text2) TextView textView2;
+    @BindView(R.id.text3) TextView textView3;
+    @BindView(R.id.text4) TextView textView4;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        ButterKnife.bind(this);
         setToolbar();
+        changeTextFont();
     }
 
     private void setToolbar() {
@@ -38,6 +58,25 @@ public class SignInActivity extends LocalizationActivity {
         toolbar.setTitle("");
         toolbar.setSubtitle("");
     }
+
+    private void changeTextFont(){
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/normal.ttf");
+        //Typeface fontBold = Typeface.createFromAsset(getAssets(), "fonts/bold.ttf");
+
+        radioButton1.setTypeface(font);
+        radioButton2.setTypeface(font);
+
+        userNameInput.setTypeface(font);
+        passwordInput.setTypeface(font);
+
+        loginButton.setTypeface(font);
+        textView1.setTypeface(font);
+        textView2.setTypeface(font);
+        textView3.setTypeface(font);
+        textView4.setTypeface(font);
+
+    }
+
 
 
 }
