503cfee3994889dfb0af5e3a75581a3711ccb52f
mostafa_anter <ga30000@gmail.com>
2016-12-05 11:14:40 +0200

    stable version

--- app/src/main/java/com/perfect_apps/koch/activities/ChangePasswordActivity.java
+++ app/src/main/java/com/perfect_apps/koch/activities/ChangePasswordActivity.java
@@ -1,17 +1,22 @@
 package com.perfect_apps.koch.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.perfect_apps.koch.R;
+import com.perfect_apps.koch.utils.SweetDialogHelper;
+import com.perfect_apps.koch.utils.Utils;
 
 import butterknife.ButterKnife;
 
 public class ChangePasswordActivity extends AppCompatActivity {
+
+    private static ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +24,36 @@ public class ChangePasswordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_change_password);
         ButterKnife.bind(this);
         setToolbar();
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        WebView wv1 = (WebView) findViewById(R.id.webview);
+        progressBar.setVisibility(View.GONE);
 
+        if (Utils.isOnline(this)) {
+            progressBar.setVisibility(View.VISIBLE);
+            wv1.setWebViewClient(new MyBrowser());
+            wv1.getSettings().setLoadsImagesAutomatically(true);
+            wv1.getSettings().setJavaScriptEnabled(true);
+            wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+            wv1.loadUrl("http://services-apps.net/koch/ar/password/reset");
+        } else {
+            // show error message
+            new SweetDialogHelper(this).showErrorMessage(getString(R.string.error),
+                    getString(R.string.there_is_no_internet));
+        }
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(View.GONE);
+        }
     }
 
     private void setToolbar() {
